3ef7bda1b70bbeb31ce2330b0f87bac77679afff
Bruno <bruno@garciaechegaray.com>
2016-04-02 21:14:31 +0200

    Testing immutable library

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ http://teropa.info/blog/2015/09/10/full-stack-redux-tutorial.html
 `apm install linter`
 
 `apm install linter-eslint`
+
+## Run test
+
+`npm run test`
--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,6 +1,8 @@
 import {expect} from 'chai';
-describe('immutability', () => {
-  describe('a number', () => {
+import {List, Map} from 'immutable';
+
+describe('Immutability', () => {
+  describe('A number', () => {
     function increment (currentState) {
       return currentState + 1;
     }
@@ -9,6 +11,55 @@ describe('immutability', () => {
       let nextState = increment(state);
       expect(nextState).to.equal(43);
       expect(state).to.equal(42);
+    });
+  });
+
+  describe('A List', () => {
+    function addMovie (currentState, movie) {
+      return currentState.push(movie);
+    }
+
+    it('is immutable', () => {
+      let state = List.of('Trainspotting', '28 Days Later');
+      let nextState = addMovie(state, 'Sunshine');
+
+      expect(nextState).to.equal(List.of(
+        'Trainspotting',
+        '28 Days Later',
+        'Sunshine'
+      ));
+
+      expect(state).to.equal(List.of(
+        'Trainspotting',
+        '28 Days Later'
+      ));
+    });
+  });
+
+  describe('A tree', () => {
+    function addMovie (currentState, movie) {
+      return currentState.update('movies', (movies) => movies.push(movie));
+    }
+
+    it('is immutable', () => {
+      let state = Map({
+        movies: List.of('Trainspotting', '28 Days Later')
+      });
+      let nextState = addMovie(state, 'Sunshine');
+
+      expect(nextState).to.equal(Map({
+        movies: List.of(
+          'Trainspotting',
+          '28 Days Later',
+          'Sunshine'
+        )
+      }));
+      expect(state).to.equal(Map({
+        movies: List.of(
+          'Trainspotting',
+          '28 Days Later'
+        )
+      }));
     });
   });
 });
