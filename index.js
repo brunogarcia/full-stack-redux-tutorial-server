10961c48401eb47e6e52a54c17216534d51f392a
Bruno <bruno@garciaechegaray.com>
2016-04-03 11:40:12 +0200

    Receiving Remote Redux Actions

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import makeStore from './src/store';
-import startServer from './src/server';
+import {startServer} from './src/server';
 
 export const store = makeStore();
-startServer();
+
+startServer(store);
+
+store.dispatch({
+  type: 'SET_ENTRIES',
+  entries: require('./entries.json')
+});
+
+store.dispatch({type: 'NEXT'});
